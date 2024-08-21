6bc8e90b1daae80f961c15dd044f8064e381925c
Esraa_Shalan <123131465+shalan20@users.noreply.github.com>
2024-08-21 11:47:06 +0300

    Add files via upload

---
+++ car.cpp
@@ -0,0 +1,11 @@
+﻿#include "car.h"
+int main()
+{
+	car c1;//استدعيت ال default constructor
+	cout << "color = " << c1.getcolor() << endl;
+	cout << "model = " << c1.getmodel() << endl;
+
+	car c2('b',2023); //استدعيت ال constructor using parameters 
+	cout << "color = " << c2.getcolor() << endl;
+	cout << "model = " << c2.getmodel() << endl;
+}
---
+++ car.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+#include<iostream>
+using namespace std;
+class car
+{
+private:
+	char color;
+	int model;
+public:
+	//constructor overloading first constructor
+	car() //constructor instead of setter
+	{
+		color = 'r';
+		model = 2019;
+	}
+	/* constructor using intialization list
+	car():color('r'),model(2019)
+	{
+		cout << "the color and model are intialized";
+	}*/
+
+
+	// constructor  هدخله القيم فى ال main
+	//constructor overloading second constructor
+	car(char c, int m)
+	{
+		color = c;
+		model = m;
+	}
+	char getcolor()
+	{
+		return color;
+	}
+	int  getmodel()
+	{
+		return model;
+	}
+
+
+};
+
